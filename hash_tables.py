e37aefa7e88241ca5636da2434866a01463f3f4e
icedrunkard <icedrunkard@163.com>
2018-06-10 21:08:20 +0800

    first commit

--- hash_tables.py
+++ hash_tables.py
@@ -10,13 +10,13 @@ class Hashtable():
     def rehash(self, old_hash):
         return (old_hash + 1) % self.size
 
-    def put(self, key, value):
+    def insert(self, key, value):
         first_hashed = self.first_hash(key)
-        if self.keys[first_hashed] == None:
+        if not self.keys[first_hashed]:
             self.keys[first_hashed] = key
             self.values[first_hashed] = value
         else:
-            rehashed = self.rehash(key)
+            rehashed = self.rehash(first_hashed)
             while not self.keys[rehashed] and \
                     self.keys[rehashed] != key:  # 如果键已经存在就覆盖
                 rehashed = self.rehash(key)
@@ -25,5 +25,23 @@ class Hashtable():
             self.keys[rehashed] = key
             self.values[rehashed] = value
 
-    def get(self,key):
-        pass
+    def get(self, key):
+        first_hashed = self.first_hash(key)
+        if self.keys[first_hashed] == key:
+            return self.values[first_hashed]
+        else:
+            rehashed = self.rehash(first_hashed)
+            while self.keys[rehashed] != key:
+                if rehashed == first_hashed:
+                    break
+                rehashed = self.rehash(rehashed)
+            if self.keys[rehashed] == key:
+                return self.values[rehashed]
+            else:
+                raise ValueError('key {} not found'.format(key))
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.insert(key, value)
